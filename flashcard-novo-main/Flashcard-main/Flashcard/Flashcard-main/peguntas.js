6aef93aeafec03b6eef3fd725163cd27849054ba
tanakiryus <148972044+vic23tanakiryus@users.noreply.github.com>
2024-11-13 12:13:09 -0300

    Add files via upload

---
+++ flashcard-novo-main/Flashcard-main/Flashcard/Flashcard-main/peguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    "Programação",
+    "O que é Java Script?",
+    "O Java Script é um linguagem de programação"
+)
+
+criaCartao(
+    "Geografia",
+    "Qual a capital da França?",
+    "A capital da França é Paris"
+)
+
+criaCartao(
+    "Conhecimento Geral",
+    "Quantas variedades de cerejas existem no mundo?",
+    "Existem 1000 diferentes registradas até hoje."
+)
+
+criaCartao(
+    "Matemática",
+    "Quais são os quatro principais sinais da matemática?",
+    "Soma, subtração, multiplicação e divisão "
+)
